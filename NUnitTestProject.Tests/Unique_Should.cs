51c1b8b24d395daf73ad55e9234fcb1892c5de80
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:19:45 +0200

    add unit tests for Unique method

---
+++ NUnitTestProject.Tests/Unique_Should.cs
@@ -0,0 +1,32 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace NUnitTestProject.Tests
+{
+    public class Unique_Should
+    {
+        [Test]
+        public void ReturnsAllUniqueItems()
+        {
+            var arr = new[] { 1, 2, 6, 3, 2 };
+            var expected = new[] { 1, 2, 6, 3 };
+
+            var result = arr.Unique();
+
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Unique();
+            });
+        }
+    }
+}
