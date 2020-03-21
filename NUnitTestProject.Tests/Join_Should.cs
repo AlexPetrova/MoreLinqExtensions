42be9c9533365a7a583206d1f6824aa4b8dff343
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:18:34 +0200

    add unit tests for Join method

---
+++ NUnitTestProject.Tests/Join_Should.cs
@@ -0,0 +1,53 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTestProject.Tests
+{
+    public class Join_Should
+    {
+        [Test]
+        public void ReturnJoinedElements()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+            var expected = "1, 2, 3, 4";
+
+            var result = arr.Join(", ");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ReturnEmptyString_EmptyCollection()
+        {
+            var arr = new int[0];
+            var expected = "";
+
+            var result = arr.Join(", ");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void NoSeparatorAtEnd_OneElement()
+        {
+            var arr = new[] { 1 };
+            var expected = "1";
+
+            var result = arr.Join(", ");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Join("");
+            });
+        }
+    }
+}
