27dea6890a8156b969e635e2a01a3ff03856b78c
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:18:45 +0200

    add unit tests for Limit method

---
+++ NUnitTestProject.Tests/Limit_Should.cs
@@ -0,0 +1,42 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace NUnitTestProject.Tests
+{
+    public class Limit_Should
+    {
+        [Test]
+        public void ReturnsLimitCountOfElements()
+        {
+            var arr = new[] { 1, 2, 6, 3, 2 };
+            var expected = new[] { 1, 2 };
+
+            var result = arr.Limit(2);
+
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void ReturnAll_LimitBiggerThanLenght()
+        {
+            var arr = new[] { 1, 2, 6, 3, 2 };
+
+            var result = arr.Limit(10);
+
+            Assert.IsTrue(arr.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Limit(1);
+            });
+        }
+    }
+}
