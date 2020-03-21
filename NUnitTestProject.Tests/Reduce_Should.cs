ed0399363401e35623355a475f92e5b867e4d620
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:19:26 +0200

    add unit tests for Reduce method

---
+++ NUnitTestProject.Tests/Reduce_Should.cs
@@ -0,0 +1,31 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTestProject.Tests
+{
+    public class Reduce_Should
+    {
+        [Test]
+        public void ApplyReducer()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+            var expected = 10;
+
+            var result = arr.Reduce((current, initialValue) => current + initialValue, 0);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Reduce((x, y) => x, 0);
+            });
+        }
+    }
+}
