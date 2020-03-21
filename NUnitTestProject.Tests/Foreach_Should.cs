b88d917c0a84078b99269cdcc8cee62debba45a6
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:18:19 +0200

    add unit tests for Foreach method

---
+++ NUnitTestProject.Tests/Foreach_Should.cs
@@ -0,0 +1,31 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTestProject.Tests
+{
+    public class Foreach_Should
+    {
+        [Test]
+        public void ApplyAction()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+            var count = 0;
+
+            arr.Foreach(x => count++);
+
+            Assert.AreEqual(4, count);
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Foreach(x => x++);
+            });
+        }
+    }
+}
