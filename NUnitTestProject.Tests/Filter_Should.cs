7e40c0d7336238a3e9bbb2ef80abf7e319484f82
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:17:55 +0200

    add unit tests for Filter method

---
+++ NUnitTestProject.Tests/Filter_Should.cs
@@ -0,0 +1,46 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace NUnitTestProject.Tests
+{
+    public class Filter_Should
+    {
+        [Test]
+        public void ApplyFilter()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+            var expected = new[] { 2, 4 };
+
+            var result = arr.Filter(x => x % 2 == 0);
+
+            Assert.IsTrue(result.SequenceEqual(expected));
+        }
+
+        [Test]
+        public void BeLazy()
+        {
+            var arr = new[] { 1, 2, 3, 4 };
+            var count = 0;
+
+            var result = arr.Filter(x => {
+                count++;
+                return x % 2 == 0;
+            });
+
+            Assert.AreEqual(0, count);
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Filter(x => x == 1);
+            });
+        }
+    }
+}
