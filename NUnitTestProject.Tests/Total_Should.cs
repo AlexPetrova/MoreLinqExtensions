b29060f9c30d8dbc99b2332b61e9782744a2f38c
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:19:34 +0200

    add unit tests for Total method

---
+++ NUnitTestProject.Tests/Total_Should.cs
@@ -0,0 +1,40 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTestProject.Tests
+{
+    public class Total_Should
+    {
+        [Test]
+        public void ReturnsTotalCountOfMatch()
+        {
+            var arr = new[] { 1, 2, 6, 3, 2 };
+
+            var result = arr.Total(x => x % 2 == 0);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void ReturnsZero_WhenNoMatch()
+        {
+            var arr = new[] { 1, 2, 3 };
+
+            var result = arr.Total(x => x == 4);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.Total(x => true);
+            });
+        }
+    }
+}
