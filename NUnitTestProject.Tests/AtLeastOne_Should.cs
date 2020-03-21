de5997bb50b43fc0d0c5119440c0889f62477adc
AlexPetrova <a.petrova95@yahoo.com>
2020-03-21 20:17:44 +0200

    add unit tests for AtLeastOne method

---
+++ NUnitTestProject.Tests/AtLeastOne_Should.cs
@@ -0,0 +1,46 @@
+﻿using LinqMoreExtensions;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTestProject.Tests
+{
+    public class AtLeastOne_Should
+    {
+        [Test]
+        public void ReturnOnFirstOccurrence()
+        {
+            var arr = new[] { 1, 2, 6, 3, 2 };
+            var index = -1;
+
+            var result = arr.AtLeastOne(x =>
+            {
+                index++;
+                return x == 2;
+            });
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(1, index);
+        }
+
+        [Test]
+        public void ReturnFalse_NoOccurrence()
+        {
+            var arr = new[] { 1, 2, 6, 3, 2 };
+
+            var result = arr.AtLeastOne(x => x == 10);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Throw_WhenSourceIsNull()
+        {
+            int[] arr = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                arr.AtLeastOne(x => true);
+            });
+        }
+    }
+}
